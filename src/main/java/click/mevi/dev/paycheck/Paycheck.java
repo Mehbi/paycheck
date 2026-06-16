d3a86f652e437fc252771a12f9df8b7717132a9d
mehbi <diegme81@gmail.com>
2026-06-16 00:08:49 -0300

    First commit

---
+++ src/main/java/click/mevi/dev/paycheck/Paycheck.java
@@ -0,0 +1,16 @@
+package click.mevi.dev.paycheck;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Paycheck extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
